4aa96f53a6098f15baa50fe62fb0f83f9134f2cb
achujoseph0709 <achujozefsl0709@gmail.com>
2023-08-08 15:27:02 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# User-Management-using-React
+Entirely build with Python, Django, and React to develop a basic idea in React,Redux,and Rest Api.
---
+++ frontend/frontend/src/axios.jsx
@@ -0,0 +1,17 @@
+import axios from "axios"
+
+// let token = JSON.parse(localStorage.getItem('authorization.user'))
+
+const instance = axios.create({
+  baseURL: 'http://localhost:8000',
+  headers:{
+    Authorization:localStorage.getItem('access_token')? 'JWT '+localStorage.getItem('access_token'):null,
+    'Content-Type':'application/json',
+    accept:'application/json'
+  }
+})
+
+export default instance
+
+
+//bearer token
